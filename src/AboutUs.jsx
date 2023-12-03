cf0b6d1415535cb9e5e066fc1b29d6601585f075
Nafis <bhuiyannafis517@gmail.com>
2023-12-03 12:41:00 +0600

    about

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -2,14 +2,14 @@ import { FaSquareCheck } from "react-icons/fa6";
 const AboutUs = () => {
     return (
         <div>
-            <div>
-                <div>
-
+            <div className="grid mx-3 flex-row-reverse md:grid-cols-2 gap-4">
+                <div >
+                  <img className="w-full h-80" src="https://i.postimg.cc/QC2HDLtD/download-13.jpg" alt="" />
                 </div>
-                <div>
+                <div className="align-middle my-10">
                     <h1 className="text-green-600 text-2xl text-semibold">About Us</h1>
                     <h1 className="text-4xl border-b-4  text-bold">Repairs are our passion</h1>
-                    <p className="border-4 w-8 border-green-700"></p>
+                    <p className="border-4 w-24 border-green-700"></p>
                     <h1 className="text-gray-500">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</h1>
                     <div className="grid grid-cols-2">
                         <div>
